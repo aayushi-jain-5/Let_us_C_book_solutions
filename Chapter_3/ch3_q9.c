be9071550d41919d637cb632b8be23c21e4cb12f
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2023-03-07 10:30:18 +0530

    Create ch3_q9.c

---
+++ Chapter_3/ch3_q9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,p,l=5,b=4;
+	p=2*(l+b);
+	a=l*b;
+	if(a>p)
+	 printf("Area is greater than the perimeter");
+	else
+	 printf("Area is not greater than the perimeter");
+	return 0; 
+}
