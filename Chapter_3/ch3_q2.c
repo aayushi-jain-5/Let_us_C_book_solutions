f1ce6a01da407c634c4d458ce8b50a4bb62a55df
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2023-03-07 10:24:47 +0530

    Create ch3_q2.c

---
+++ Chapter_3/ch3_q2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the number : ");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("Even Number");
+	}
+	else
+	{
+		printf("Odd Number");
+	}
+	return 0;
+}
